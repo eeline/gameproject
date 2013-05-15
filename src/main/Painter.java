3ce83f7dda497580e80e24dcd7320bed78464fb4
eeline <eelinei@gmail.com>
2013-05-15 19:49:23 -0400

    added documentation to Painter

--- src/main/Painter.java
+++ src/main/Painter.java
@@ -13,22 +13,45 @@ public class Painter {
 	private Painter(ImageObserver ob){
 		this.ob = ob;
 	}
-	
-	public static void init(ImageObserver ob){
+	/**
+	 * should only be called in main loop at start
+	 * @param ob
+	 */
+	protected static void init(ImageObserver ob){
 		if(instance == null)
 			instance = new Painter(ob);
 	}
 	
+	/**
+	 * should be called every time a buffer is deployed
+	 * @param g
+	 */
+	protected static void setG(Graphics g){
+		instance.g = g;
+	}
+	
+	/**
+	 * paints an arbitrary image
+	 * @param image
+	 * @param x
+	 * @param y
+	 */
 	public static void paint(Image image, int x, int y){
 		instance.g.drawImage(image, x, y, instance.ob);
 	}
-	
+	/** 
+	 * <b> BUG</b> currently color setting does not affect painted color
+	 * paints an arbitrary quadrilateral
+	 * @param x
+	 * @param y
+	 * @param width
+	 * @param height
+	 * @param c
+	 */
+	//TODO fix color bug
 	public static void paint(int x, int y, int width, int height, Color c){
 		instance.g.setColor(c);
 		instance.g.drawRect(x, y, width, height);
 	}
-	
-	public static void setG(Graphics g){
-		instance.g = g;
-	}
+
 }
